d05d3961ba713573986b981a585e5893fd7d601b
Renata Arruda <renata.arruda85@gmail.com>
2023-07-19 16:13:44 -0300

    refactor: integrando as funções ao banco de dados

--- mulheres.js
+++ mulheres.js
@@ -1,99 +1,79 @@
 const express = require('express') // iniciando o express
 const router = express.Router() // configurando a primeira parte da rota
-const { v4: uuidv4 } = require('uuid')
 
-const conectaBancoDeDados = require('./bancoDeDados') //ligando ao arquivo bancoDeDados
+const conectaBancoDeDados = require('./bancoDados') //ligando ao arquivo bancoDeDados
 conectaBancoDeDados() //chamando a função que conecta o banco de dados
 
+const Mulher = require('./mulherModel')
 const app = express() // iniciando o app
 app.use(express.json())
 const porta = 3333; // criando a porta
 
-// criando a lista inicial de mulheres
-const mulheres = [
-    {
-        id: '1',
-        nome: 'Renata Arruda',
-        imagem: 'https://encurtador.com.br/vCSV0',
-        minibio: 'Estagiária de desenvolvimento de software'
-    },
-    {
-        id: '2',
-        nome: 'Iana Chan',
-        imagem: 'https://encurtador.com.br/mLNPQ',
-        minibio: 'Fundadora da Programaria'
-    },
-    {
-        id: '3',
-        nome: 'Nina da Hora',
-        imagem: 'https://encurtador.com.br/eoIQY',
-        minibio: 'Hacker antirracista'
-    },
-    {
-        id: '4',
-        nome: 'Simara Conceição',
-        imagem: 'https://encurtador.com.br/hpIY3',
-        minibio: 'Desenvolvedora e instrutora'
-    }
-]
-
 // GET
-function mostraMulheres(request, response){
-    response.json(mulheres)
+async function mostraMulheres(request, response){
+    try{
+        const mulheresVindasDoBancoDeDados = await Mulher.find()
+
+        response.json(mulheresVindasDoBancoDeDados)
+    } catch (erro){
+        console.log(erro)
+    }
 }
 
 // POST
-function criarMulher(request, response) {
-    const novaMulher = {
-        id: uuidv4(),
+async function criarMulher(request, response) {
+    const novaMulher = new Mulher({
         nome: request.body.nome,
         imagem: request.body.imagem,
-        minibio: request.body.minibio
+        minibio: request.body.minibio,
+        citacao: request.body.citacao
+    })
+
+    try {
+        const mulherCriada = await novaMulher.save()
+        response.status(201).json(mulherCriada)
+    } catch (erro) {
+        console.log(erro)
     }
-
-    mulheres.push(novaMulher);
-
-    response.json(mulheres);
 }
 
 //PATCH
-function corrigeMulher(request, response){
-
-    function encontraMulher(mulher){
-        if(mulher.id === request.params.id){
-            return mulher
+async function corrigeMulher(request, response){
+    try {
+        const mulherEncontrada = await Mulher.findById(request.params.id)
+        if (request.body.nome){
+            mulherEncontrada.nome = request.body.nome
         }
+    
+        if (request.body.imagem){
+            mulherEncontrada.imagem = request.body.imagem
+        }
+    
+        if (request.body.minibio){
+            mulherEncontrada.minibio = request.body.minibio
+        }
+        if (request.body.citacao){
+            mulherEncontrada.citacao = request.body.citacao
+        }
+
+        const mulherAtualizadaNoBancoDeDados = await mulherEncontrada.save()
+        response.json(mulherAtualizadaNoBancoDeDados)
+    } catch (erro){
+        console.log(erro)
     }
-
-    const mulherEncontrada = mulheres.find(encontraMulher)
-
-    if (request.body.nome){
-        mulherEncontrada.nome = request.body.nome
-    }
-
-    if (request.body.imagem){
-        mulherEncontrada.imagem = request.body.imagem
-    }
-
-    if (request.body.minibio){
-        mulherEncontrada.minibio = request.body.minibio
-    }
-
-    response.json(mulheres)
 }
 
 //DELETE
-function deletaMulher(request, response){
+async function deletaMulher(request, response){
 
-    function todasMenosEla(mulher){
-        if(mulher.id !== request.params.id){
-            return mulher
-        }
+    try {
+
+        await Mulher.findByIdAndDelete(request.params.id)
+        response.json({ mensagem: 'Mulher deletada com sucesso!'})
+
+    } catch (erro){
+        console.log(erro)
     }
-
-    const mulherQueFicam = mulheres.filter(todasMenosEla)
-
-    response.json(mulherQueFicam)
 }
 
 // PORTA
